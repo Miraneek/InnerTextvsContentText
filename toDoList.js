2b0e5a249a1e9eddd767b0b34dc1f6ea9192ba4a
Miraneek <surykata05@gmail.com>
2023-12-19 13:18:59 +0100

    without edit button, forgot previous project

---
+++ toDoList.js
@@ -0,0 +1,35 @@
+let button = document.querySelector('.add')
+button.addEventListener("click", add);
+
+let input = document.querySelector("#input")
+input.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+        add();
+    }
+});
+
+function add() {
+    if (document.querySelector("#input").value === "") {
+        return;
+    }
+    let output = document.createElement("div");
+    output.classList.add("output");
+    let name = document.createElement("h4")
+    name.innerText = document.querySelector("#input").value
+
+    output.appendChild(name);
+
+    let btn = document.createElement("button");
+    btn.innerText = "Remove";
+    output.appendChild(btn);
+
+    document.querySelector("#tasks").appendChild(output);
+    document.querySelector("#input").value = "";
+    document.querySelector("#tasks").appendChild(document.createElement("hr"));
+
+    btn.addEventListener("click", function(){
+        document.querySelector("#tasks").removeChild(output);
+        document.querySelector("hr").remove();
+    })
+
+}
